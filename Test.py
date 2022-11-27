b2a7964df2730202eccbb07fb3eecb8c5688c5a2
Krystian-Key <kluczkiewicz1512@wp.pl>
2022-11-27 14:52:02 +0100

    Initial commit

---
+++ Test.py
@@ -0,0 +1,65 @@
+print("Hello I From Somalia")
+
+# komentarz
+
+# mnożenie i dodawanie
+
+print(2 + 2)
+print(3 * 3)
+print(3 + 2 * 2)
+print(4 - 2 * 2)
+
+# dzielenie
+
+10 / 3
+4 / 2
+
+# ilość dzielenia bez przeczinka
+
+10 // 3
+7 // 6
+10 // 3
+
+# potęgowanie
+
+2 ** 5
+
+# operator modulo ( dzielenie ) zwraca resztę
+
+10 % 3
+12 % 5
+10 % 2
+11 % 2
+
+# działanie
+
+(10 - 2 ** 3) * 10
+
+#%%
+
+a = 10
+b = 20
+
+c = a * b
+print(c)
+
+#%%
+print('love python')
+print("love python")
+
+#%%
+print("It's the best!")
+# 'It's the best!' błąd
+print('It\'s the best!')
+
+#%%
+print('Python 3.7')
+
+# w nowej linii \n
+print('Python\n3.7')
+
+# w nowej linii """ ... """
+print("""Python 3.7""")
+
+#%%
+
